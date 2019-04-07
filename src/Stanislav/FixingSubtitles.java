b126d07fb0a6ee1e1d2ca685002d572d3cf891b6
Sachko151 <sachko151@abv.bg>
2019-04-07 22:38:39 +0300

    The Logic needs to be changed and the code should be cleande up!

---
+++ src/Stanislav/FixingSubtitles.java
@@ -0,0 +1,64 @@
+package Stanislav;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+public class FixingSubtitles {
+    private static final int ADDITION = 2;
+    private static final String INPUT_FILE = "src/Bora.sub.sub";
+    private static final String OUTPUT_FILE = "fixed.sub";
+
+    public static void main(String[] args) {
+        Scanner fileInput = null;
+        PrintStream fileOutput = null;
+        try {
+            // Create scanner with the Cyrillic encoding
+            fileInput = new Scanner(new File(INPUT_FILE), "windows-1251");
+
+            fileOutput = new PrintStream(OUTPUT_FILE, "windows-1251");
+            String line;
+            while (fileInput.hasNextLine()) {
+                line = fileInput.nextLine();
+                String fixedLine = fixLine(line);
+                fileOutput.println(fixedLine);
+            }
+        } catch (FileNotFoundException fnfe) {
+            System.err.println(fnfe.getMessage());
+        } catch (UnsupportedEncodingException uee) {
+            System.err.println(uee.getMessage());
+        } finally {
+            if (null != fileInput) {
+                fileInput.close();
+            }
+            if (null != fileOutput) {
+                fileOutput.close();
+            }
+        }
+    }
+
+    private static String fixLine(String line) {
+        // Find closing brace
+        int bracketFromIndex = line.indexOf('}');
+        // Extract 'from' time
+        String fromTime = line.substring(1,bracketFromIndex);
+        // Calculate new 'from' time
+        int newFromTime = (((Integer.parseInt(fromTime)/60)*1000 + ADDITION)/1000)*60;
+        System.out.println(newFromTime);
+        // Find the following closing brace
+        int bracketToIndex = line.indexOf('}', bracketFromIndex + 1);
+
+        // Extract 'to' time
+        String toTime = line.substring(bracketFromIndex + 2, bracketToIndex);
+
+        // Calculate new 'to' time
+        int newToTime = (Integer.parseInt(toTime)/60)*1000+ADDITION;
+
+        // Create a new line using the new 'from' and 'to' times
+        String fixedLine = "{" + newFromTime + "}" + "{" + newToTime + "}" + line.substring(bracketToIndex + 1);
+
+        return fixedLine;
+    }
+}
---
+++ src/Stanislav/Gui.java
@@ -0,0 +1,139 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+
+
+public class Gui {
+
+    static JButton loadFileButton;
+    JButton openFileButton;
+    static JButton exitButton;
+    static JButton submitButton;
+    ButtonGroup buttonGroup;
+    JRadioButton fastRadioButton, slowRadioButton;
+    static JCheckBox removeTagButton;
+    static JTextField textField;
+    JLabel textLabel;
+    JFileChooser fileChooser;
+    static JTextArea log;
+
+
+    Gui() {
+        JFrame frame = new JFrame("Редактор на субтитри");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        loadFileButton = new JButton("Зареди файл");
+        openFileButton = new JButton("Отвори файл");
+        exitButton = new JButton("Изход");
+        exitButton = new JButton("Изход");
+        submitButton = new JButton("Запиши промените");
+        fastRadioButton = new JRadioButton("Забързай с");
+        slowRadioButton = new JRadioButton("Забави с");
+        buttonGroup = new ButtonGroup();
+        buttonGroup.add(fastRadioButton);
+        buttonGroup.add(loadFileButton);
+        textField = new JTextField();
+        textLabel = new JLabel();
+        log = new JTextArea(5, 20);
+
+        loadFileButton.setBounds(80, 30, 200, 50);
+        openFileButton.setBounds(80, 90, 200, 50);
+        openFileButton.setBackground(Color.YELLOW);
+        exitButton.setBounds(80, 150, 200, 50);
+        exitButton.setBackground(Color.red);
+        textField.setBounds(350, 100, 50, 50);
+        submitButton.setBounds(80, 210, 200, 50);
+        submitButton.setBackground(Color.GREEN);
+        fastRadioButton.setBounds(350, 30, 100, 50);
+        slowRadioButton.setBounds(350, 60, 100, 50);
+        removeTagButton = new JCheckBox("Премахни таговете от текста");
+        removeTagButton.setBounds(350, 160, 200, 100);
+        log.setBounds(80, 270, 200, 50);
+
+        frame.add(loadFileButton);
+        frame.add(openFileButton);
+        frame.add(exitButton);
+        frame.add(submitButton);
+        frame.add(fastRadioButton);
+        frame.add(slowRadioButton);
+        frame.add(textField);
+        frame.add(log);
+        frame.add(removeTagButton);
+        frame.setSize(700, 400);
+        frame.setLayout(null);
+        frame.setResizable(true);
+        frame.setVisible(true);
+    }
+
+    public static void setExitButton() {
+
+        exitButton.addActionListener(e -> {
+            System.exit(0);
+        });
+    }
+
+    public static void setLoadFileButton() {
+        loadFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+
+                showOpenFileDialog();
+
+            }
+
+        });
+    }
+
+    private static void showOpenFileDialog() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("Subtitle Documents.srt", "srt"));
+        fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("Subtitle Documents.sub", "sub"));
+        fileChooser.setAcceptAllFileFilterUsed(true);
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+
+            log.append("Зареждам файл: " + selectedFile.getName() + ".");
+        } else {
+            log.append("Зареждане на файл отказано от потребителя.");
+        }
+    }
+
+    public static void setSubmitButton() {
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String textMs = textField.getText().trim();
+
+                if ((!textMs.isEmpty()) && (textMs != null)) {
+                    int addition = Integer.parseInt(textMs);
+                    System.out.println(addition);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Моля въведете число за милисекунди");
+                }
+            }
+        });
+    }
+
+    public static void setRemoveTagButton() {
+        removeTagButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (removeTagButton.isSelected()) {
+                    String text = "<B>I don't want this to be bold<\\B>";
+                    text = text.replaceAll("\\<.*?\\>", "");
+                    System.out.println(text);
+                } else {
+                    System.out.println("no");
+                }
+            }
+        });
+    }
+
+}
