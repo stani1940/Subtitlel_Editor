ef64e85a59ab253084e0b642d9c888f710b9a8ce
stani1940 <stanislav1940@abv.bg>
2019-04-15 09:17:59 +0300

    Add GUI simple design

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ public class App {
     public static void main(String[] args) {
 
         Gui gui = new Gui();
+        gui.drawAndShowGui();
         gui.setExitButton();
         gui.setLoadFileButton();
         gui.setSubmitButton();
--- src/Gui.java
+++ src/Gui.java
@@ -7,22 +7,20 @@ import java.io.File;
 
 public class Gui extends Component {
 
-    JButton loadFileButton;
-    JButton openFileButton;
-    JButton exitButton;
-    JButton submitButton;
-    ButtonGroup buttonGroup;
-    JRadioButton fastRadioButton, slowRadioButton;
-    JCheckBox removeTagButton;
-    JTextField textField;
-    JLabel textLabel;
-    JTextArea log;
+    private JButton loadFileButton;
+    private JButton openFileButton;
+    private JButton exitButton;
+    private JButton submitButton;
+    private ButtonGroup buttonGroup;
+    private JRadioButton fastRadioButton, slowRadioButton;
+    private JCheckBox removeTagButton;
+    private JTextField textField;
+    private JTextArea log;
+    private JFileChooser fileChooser = new JFileChooser();
 
-
-    Gui() {
+    void drawAndShowGui() {
         JFrame frame = new JFrame("Редактор на субтитри");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         loadFileButton = new JButton("Зареди файл");
         openFileButton = new JButton("Отвори файл");
         exitButton = new JButton("Изход");
@@ -32,24 +30,37 @@ public class Gui extends Component {
         buttonGroup = new ButtonGroup();
         buttonGroup.add(fastRadioButton);
         buttonGroup.add(slowRadioButton);
+        JLabel textLabelMSeconds = new JLabel("Mилисекунди");
         textField = new JTextField();
-        textLabel = new JLabel();
+        JLabel textLabel = new JLabel("Developed by Alexander, Venelina and Stanislav");
         log = new JTextArea(5, 20);
+        log.setFont(new Font("Arial", Font.BOLD, 20));
 
-        loadFileButton.setBounds(80, 30, 200, 50);
-        openFileButton.setBounds(80, 90, 200, 50);
+        loadFileButton.setBounds(50, 20, 250, 100);
+        loadFileButton.setFont(new Font("Arial", Font.BOLD, 25));
+        openFileButton.setBounds(50, 130, 250, 100);
         openFileButton.setBackground(Color.YELLOW);
-        exitButton.setBounds(80, 150, 200, 50);
+        openFileButton.setFont(new Font("Arial", Font.BOLD, 25));
+        exitButton.setBounds(50, 240, 250, 100);
         exitButton.setBackground(Color.red);
-        textField.setBounds(350, 100, 50, 50);
-        submitButton.setBounds(80, 210, 200, 50);
+        exitButton.setFont(new Font("Arial", Font.BOLD, 30));
+        textField.setBounds(510, 20, 50, 100);
+        textField.setFont(new Font("Arial", Font.BOLD, 40));
+        textLabelMSeconds.setBounds(570,20,150,100);
+        textLabelMSeconds.setFont(new Font("Arial", Font.BOLD, 20));
+        submitButton.setBounds(50, 350, 250, 100);
+        submitButton.setFont(new Font("Arial", Font.BOLD, 20));
         submitButton.setBackground(Color.GREEN);
-        fastRadioButton.setBounds(350, 30, 100, 50);
-        slowRadioButton.setBounds(350, 60, 100, 50);
+        fastRadioButton.setBounds(350, 20, 150, 60);
+        fastRadioButton.setFont(new Font("Arial", Font.BOLD, 20));;
+        slowRadioButton.setBounds(350, 60, 150, 60);
+        slowRadioButton.setFont(new Font("Arial", Font.BOLD, 20));
         removeTagButton = new JCheckBox("Премахни таговете от текста");
-        removeTagButton.setBounds(350, 160, 200, 100);
-        log.setBounds(80, 270, 200, 50);
-
+        removeTagButton.setBounds(350, 130, 350, 100);
+        removeTagButton.setFont(new Font("Arial", Font.BOLD, 22));
+        log.setBounds(350, 350, 350, 100);
+        textLabel.setBounds(300,500,600,50);
+        textLabel.setFont(new Font("Arial", Font.BOLD, 20));
         frame.add(loadFileButton);
         frame.add(openFileButton);
         frame.add(exitButton);
@@ -59,37 +70,29 @@ public class Gui extends Component {
         frame.add(textField);
         frame.add(log);
         frame.add(removeTagButton);
-        frame.setSize(700, 400);
+        frame.add(textLabelMSeconds);
+        frame.add(textLabel);
+        frame.setSize(800, 600);
+        frame.getContentPane().setBackground(Color.gray);
         frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
         frame.setResizable(true);
         frame.setVisible(true);
     }
 
     public void setExitButton() {
-
         exitButton.addActionListener(e -> {
             System.exit(0);
         });
-
-
     }
 
     public void setLoadFileButton() {
-        loadFileButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-
-                showOpenFileDialog();
-
-            }
-
+        loadFileButton.addActionListener(e -> {
+            showOpenFileDialog();
         });
     }
 
-    JFileChooser fileChooser = new JFileChooser();
-
     private void showOpenFileDialog() {
-
         fileChooser.setCurrentDirectory(new File(("src")));
         fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("Subtitle Documents with srt and sub", "sub", "srt"));
@@ -98,23 +101,16 @@ public class Gui extends Component {
 
         if (result == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
-
             log.append("Зареждам файл: " + selectedFile.getName() + ".");
-
         } else {
             log.append("Зареждане на файл отказано от потребителя.");
-
         }
-
     }
-
 
     private String getFileExtension(JFileChooser fileChooser) {
         String[] split = fileChooser.getSelectedFile().getName().split("\\.");
-        String ext = split[split.length - 1];
-        return ext;
+        return split[split.length - 1];
     }
-
 
     public void setSubmitButton() {
         submitButton.addActionListener(new ActionListener() {
@@ -142,9 +138,10 @@ public class Gui extends Component {
                     if (!getRadioButtonsValue()) {
                         int mSeconds = -Integer.parseInt(textMs);
                         if (getFileExtension(fileChooser).equals("sub")) {
-                            new FixingSubtitles().INPUT_FILE = String.valueOf(fileChooser.getSelectedFile());
+                            FixingSubtitles.INPUT_FILE = String.valueOf(fileChooser.getSelectedFile());
 
-                            new FixingSubtitles().ADDITION = mSeconds;
+                            FixingSubtitles.ADDITION = mSeconds;
+                            new FixingSubtitles();
                         } else if (getFileExtension(fileChooser).equals("srt")) {
                             new SrtSubtitleLogic().INPUT_FILE = String.valueOf(fileChooser.getSelectedFile());
 
@@ -164,7 +161,6 @@ public class Gui extends Component {
         removeTagButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
                 if (removeTagButton.isSelected()) {
                     String text = "<B>I don't want this to be bold<\\B>";
                     text = text.replaceAll("\\<.*?\\>", "");
@@ -176,13 +172,11 @@ public class Gui extends Component {
         });
     }
 
-    public boolean getRadioButtonsValue() {
+    private boolean getRadioButtonsValue() {
         if (fastRadioButton.isSelected()) {
             return true;
         } else {
             return false;
         }
-
     }
-
 }
